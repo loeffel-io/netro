498dcc6167fb4e57b65510d36ba9d24ff0e14c87
Lucas Löffel <lucas@loeffel.io>
2018-06-01 22:02:55 +0200

    init

---
+++ netro.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Netro
+Plugin URI: https://netro.io
+Description: The Framework for Wordpress Developers
+Version: 0.0.1
+Author: Lucas Löffel
+Author URI: https://loeffel.io
+License: MIT
+*/
+
+use \Dotenv\Dotenv;
+
+defined('ABSPATH') or die();
+
+// Important path definitions
+define('NETRO_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('NETRO_TEMPLATE_PATH', get_template_directory());
+define('NETRO_TEMPLATE_SOURCE_PATH', NETRO_TEMPLATE_PATH . '/netro/');
+define('NETRO_ENV_PATH', NETRO_TEMPLATE_PATH . '/.env');
+
+// Setup autoloader
+$loader = require_once __DIR__ . '/vendor/autoload.php';
+$loader->addPsr4('Netro\\', [
+    NETRO_TEMPLATE_SOURCE_PATH,
+    NETRO_PLUGIN_PATH . 'src/',
+]);
+
+// Load the env config file
+if (file_exists(NETRO_ENV_PATH)) {
+    (new Dotenv(NETRO_TEMPLATE_PATH))->overload();
+}
---
+++ src/helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!function_exists('asset')) {
+    /**
+     * @param string $path
+     * @return string
+     */
+    function asset(string $path): string
+    {
+        return get_template_directory_uri() . '/assets/' . $path;
+    }
+}
+
+if (!function_exists('env')) {
+    /**
+     * @param string $name
+     * @param string|null $default
+     * @return string
+     */
+    function env(string $name, string $default = null): string
+    {
+        return getenv($name) ? getenv($name) : $default;
+    }
+}
